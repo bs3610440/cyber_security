dc0fd59efbfbcec62fd95f3725840e58116cdbd8
Simran <bs3610440@gmail.com>
2025-11-03 14:17:44 +0530

    className

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Footer from './component/Footer'
 export default function App() {
   return (
     <div className='text-white'>
-<div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>  
+<div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>  
 
 <Navbar/>
 <Hero/>
